07862b514df71fd51f91a9fecfa670ab96e86670
luogeger <xiaoqing1232@163.com>
2019-05-23 22:50:40 +0800

    init, demo.jar是可执行的jar

---
+++ security-demo/src/main/java/com/youyou/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.youyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class);
+    }
+
+
+    @GetMapping(value = "demo")
+    public String demo () {
+
+        return "hello security demo 123";
+    }
+
+
+}
